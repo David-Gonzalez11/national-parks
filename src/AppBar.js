7eb130ae1029a067b9919ef9e7a963c2c2038a42
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2024-05-02 22:42:12 -0700

    Working on search bar and UI of application

--- src/AppBar.js
+++ src/AppBar.js
@@ -6,11 +6,11 @@ import Typography from "@mui/material/Typography"
 import Button from "@mui/material/Button"
 import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
-
+import TextField from "@mui/material/TextField"
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" style={{ backgroundColor: "black" }}>
         <Toolbar>
           <IconButton
             size="large"
@@ -18,18 +18,20 @@ export default function ButtonAppBar() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            style={{ backgroundColor: "black" }}
           >
             <MenuIcon />
           </IconButton>
-          <img
-            src="https://www.researchgate.net/publication/320011942/figure/fig1/AS:586733053837312@1516899469104/Redesigned-National-Park-Service-logo.png"
-            width="20%"
-          />
+          <img src="https://www.researchgate.net/publication/320011942/figure/fig1/AS:586733053837312@1516899469104/Redesigned-National-Park-Service-logo.png" />
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Home
+            National Park Service
           </Typography>
-          <Button color="inherit">Login</Button>
+            <TextField variant="outlined" label="Search" style={{backgroundColor:"white", borderRadius: "20px"}} >
+              Search
+            </TextField>
+
+          <Button color="inherit">Menu</Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/GetData.js
+++ src/GetData.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react"
 // import Box from "@mui/material-box"
 const GetData = () => {
   const [image, setImage] = useState()
-  const [parkData, setParkData] = useState([]);
+  const [parkData, setParkData] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,13 +13,12 @@ const GetData = () => {
         `https://developer.nps.gov/api/v1/parks?api_key=${apiKey}`
       )
       const data = await getRequest.json()
-            setParkData(data.data); // Assuming the data structure contains a 'data' property
+      setParkData(data.data) // Assuming the data structure contains a 'data' property
 
       console.log(data)
     }
     fetchData()
   }, [])
-
 
   const handleClick = async () => {
     const apiKey = "ACTtwg9kV0YavegYOa6xKy3ATw2aYmnjdEnOEf6j" // Replace "YOUR_API_KEY" with your actual API key
@@ -32,24 +31,25 @@ const GetData = () => {
   }
   return (
     <div>
-      {parkData.map((park,) => {
-return (
-  <div key={park.id}>
-
-<h2>{park.fullName}</h2>
-<div>
-  {park.images.map((images) => {
-    return (
-    <img key={images.url} src={images.url} alt="ParkImage" />
-
-    )
-  })}
-  </div>
-  </div>
-)
+      {parkData.map((park) => {
+        return (
+          <div key={park.id}>
+            <div>
+              {park.images.length > 0 && (
+                <img
+                  width="200px"
+                  height="100%"
+                  key={park.images[0].url}
+                  src={park.images[0].url}
+                  alt="ParkImage"
+                />
+              )}
+            </div>
+            <h2>{park.fullName}</h2>
+          </div>
+        )
       })}
-      This is the button to get data{" "}
-      <button onClick={handleClick}>Click me</button>
+      {/* <button onClick={handleClick}>Click me</button> */}
     </div>
   )
 }
