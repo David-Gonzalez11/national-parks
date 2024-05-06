2ccb547ca0bafc1736d7c37899f4de397f33061c
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2024-05-05 22:01:27 -0700

    working on search by state and other search box design

--- src/SearchParks.js
+++ src/SearchParks.js
@@ -3,27 +3,45 @@ import TextField from "@mui/material/TextField"
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
 const SearchParks = () => {
   return (
-    <div className="search-for-park">
-      <h3 style={{ marginTop: "0", marginLeft: "35px" }}>Find a Park</h3>
-      <TextField
-        style={{
-          backgroundColor: "white",
-          marginLeft: "30px",
-          marginBottom: "30px",
-          borderRadius: "20px",
-          width: "40%",
-          color:"black"
-        }}
-        label="By State..."
-                InputLabelProps={{ style: { color: "black" } }}
+    <>
+      <div className="search-for-park">
+        <div className="width-50">
+          <h3 style={{ marginTop: "0", marginLeft: "35px" }}>Find a Park</h3>
 
-      >
-
-        By State..
-
-      </TextField>
-
-    </div>
+          <TextField
+            style={{
+              backgroundColor: "white",
+              marginLeft: "30px",
+              marginBottom: "30px",
+              borderRadius: "20px",
+              width: "40%",
+              color: "black",
+            }}
+            label="By State..."
+            InputLabelProps={{ style: { color: "black" } }}
+          >
+            By State..
+          </TextField>
+        </div>
+        <div className="width-50">
+          <h3 style={{ marginTop: "0", marginLeft: "35px" }}>Find a Park</h3>
+          <TextField
+            style={{
+              backgroundColor: "white",
+              marginLeft: "30px",
+              marginBottom: "30px",
+              borderRadius: "20px",
+              width: "40%",
+              color: "black",
+            }}
+            label="By State..."
+            InputLabelProps={{ style: { color: "black" } }}
+          >
+            By State..
+          </TextField>
+        </div>
+      </div>
+    </>
   )
 }
 export default SearchParks
