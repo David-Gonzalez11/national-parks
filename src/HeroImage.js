8e8bb3bcfd2c0919e83aa971458209cb797d299a
David Gonnzalez <davidgonzalez_1999@yahoo.com>
2024-05-13 23:26:38 -0700

    Working on input fields

--- src/HeroImage.js
+++ src/HeroImage.js
@@ -3,8 +3,24 @@ import React from "react"
 const HeroImage = () => {
   return (
     <div className="hero">
-    <img src="https://reneeroaming.com/wp-content/uploads/2020/08/Map-to-visit-every-national-park-1024x711.png" alt="hero-img" className="hero-img"/>
-
+      <img
+        src="https://reneeroaming.com/wp-content/uploads/2020/08/Map-to-visit-every-national-park-1024x711.png"
+        alt="hero-img"
+        className="hero-img"
+      />
+      <div className="overlay">
+        <p>
+          <strong>
+            <h3>Asian American & Pacific Islander Month</h3>
+          </strong>
+        </p>
+        <p>
+          <strong>
+            <h3>Sharing Heritage & history</h3>
+          </strong>
+        </p>
+        <button className="overlay-button">Find Stories & Places</button>
+      </div>
     </div>
   )
 }
--- src/SearchParks.js
+++ src/SearchParks.js
@@ -1,44 +1,51 @@
-import React from "react"
+import React, { useState } from "react"
 import TextField from "@mui/material/TextField"
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown"
+import FormControl from "@mui/material/FormControl"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+
+import Select from "@mui/material/Select"
 const SearchParks = () => {
+  const [state, setState] = useState("")
+
   return (
     <>
       <div className="search-for-park">
         <div className="width-50">
           <h3 style={{ marginTop: "0", marginLeft: "35px" }}>Find a Park</h3>
 
-          <TextField
-            style={{
-              backgroundColor: "white",
-              marginLeft: "30px",
-              marginBottom: "30px",
-              borderRadius: "20px",
-              width: "40%",
-              color: "black",
-            }}
-            label="By State..."
-            InputLabelProps={{ style: { color: "black" } }}
-          >
-            By State..
-          </TextField>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label width-50">Age</InputLabel>
+            <Select
+              style={{ backgroundColor: "white" }}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={state}
+              label="Age"
+              // onChange={handleChange}
+            >
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
         </div>
-        <div className="width-50">
-          <h3 style={{ marginTop: "0", marginLeft: "35px" }}>Find a Park</h3>
-          <TextField
-            style={{
-              backgroundColor: "white",
-              marginLeft: "30px",
-              marginBottom: "30px",
-              borderRadius: "20px",
-              width: "40%",
-              color: "black",
-            }}
-            label="By State..."
-            InputLabelProps={{ style: { color: "black" } }}
+        <div className="width-50" full>
+          <InputLabel id="demo-simple-select-label width-50">Age</InputLabel>
+          <Select
+            fullWidth
+            style={{ backgroundColor: "white" }}
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={state}
+            label="Age"
+            // onChange={handleChange}
           >
-            By State..
-          </TextField>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
         </div>
       </div>
     </>
